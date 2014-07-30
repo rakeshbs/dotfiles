ddae3fff2818415c9d163f2f711aca0e79030032
Rakesh <rakesh@Rakeshs-MacBook-Pro.local>
2014-07-30 14:56:50 +0530

    Working on topcoder

--- Scripts/topcoder.rb
+++ Scripts/topcoder.rb
@@ -1,6 +1,28 @@
 require 'open-uri'
 
-TEST_URL = "http://community.topcoder.com/tc?module=MatchList&sc=&sd=&nr=200&sr=801"
+class TopcoderMatch
+  attr_accessor :title
+  attr_accessor :url
+  attr_accessor :problem
+
+  def initialize (title)
+    @title = title
+  end
+end
+
+class TopcoderProblem
+  attr_accessor :title
+  attr_accessor :url
+  attr_accessor :difficulty
+  attr_accessor :problem_statement
+
+  def initialize (title)
+    @title = title
+  end
+end
+
+# TEST_URL = "http://community.topcoder.com/tc?module=MatchList&sc=&sd=&nr=200&sr=801"
+TEST_URL = "http://community.topcoder.com/tc?module=MatchList&sc=&sd=&nr=200&sr=0"
 
 def test_topcoder
   open(TEST_URL) do |file|
